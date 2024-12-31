d0d34fdb2ca07c38d7da768602286ae75618958a
Vikram-Pavurayala <127219131+Vikram-Pavurayala@users.noreply.github.com>
2024-12-31 21:56:07 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const roomManager = new RoomManager();
 
 const io = socketIO(server, {
     cors: {
-        origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+        origin: ['http://localhost:5500', 'https://vikram-pavurayala.github.io/audioRooms/frontend/index.html'],
         methods: ["GET", "POST"]
     }
 });
